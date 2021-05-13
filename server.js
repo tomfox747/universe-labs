f370b6f97aaa19aa8ee2911d2a6ffd312eabfb85
Tom Fox <47598118+tomfox747@users.noreply.github.com>
2021-05-13 13:18:09 +0100

    update listen

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./app')
-const port = 8080
 
 // start express server on port 5000
-app.listen(8000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`server started on port ${port}`);
 });
